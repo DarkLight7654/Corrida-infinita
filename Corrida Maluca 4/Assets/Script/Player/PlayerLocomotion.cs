b436d7d13efa27a74f93e3f90f430af8a40857c4
DarkLight7654 <129552775+DarkLight7654@users.noreply.github.com>
2023-06-12 15:05:09 -0600

    V 0.5

--- Corrida Maluca 4/Assets/Script/Player/PlayerLocomotion.cs
+++ Corrida Maluca 4/Assets/Script/Player/PlayerLocomotion.cs
@@ -11,37 +11,55 @@ public class PlayerLocomotion : MonoBehaviour
     [SerializeField] private CharacterController _characterController;
 
     [SerializeField] private InputManager _inputManager;
-    [SerializeField] private float _jumpVelocity;
-    [SerializeField] private float _jumpHeight;
-    [SerializeField] private float _gravity;
+    
+    [SerializeField] private float _gravityScale = -9.81f;
+
+    [SerializeField] private float _gravityMultiplier= 1f; 
+    [SerializeField] private float _jumpForce;
+
+    private float _velocityY;
+    private Vector3 newDirection;
+    
 
     private void Update()
     {
         Walk();
-        Jump();
+        ApplyGravity();
+    }
+    private void Start() 
+    {
+        _inputManager.GameControls.Player.Jump.performed += Jump;
     }
 
     private void Walk()
     {
         Vector2 walkDirection = _inputManager.GameControls.Player.Locomotion.ReadValue<Vector2>();
 
-        Vector3 newDirection = new Vector3(walkDirection.x, 0, walkDirection.y);
+        newDirection = new Vector3(walkDirection.x, 0, walkDirection.y);
+
+        newDirection.y = _velocityY;
 
         _characterController.Move(newDirection *(_walkSpeed * Time.deltaTime));
     }
 
-    private void Jump()
+    private void ApplyGravity()
     {
-        if(_characterController.isGrounded)
+        if (_characterController.isGrounded && _velocityY < 0f)
         {
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                _jumpVelocity = _jumpHeight;
-            }
+            _velocityY = 0f;
         }
         else
         {
-            _jumpVelocity -= _gravity;
+            _velocityY += _gravityScale * _gravityMultiplier * Time.deltaTime; 
         }
     }
+
+    private void Jump(InputAction.CallbackContext ctx)
+    {
+        if(_characterController.isGrounded)
+        {
+            _velocityY += _jumpForce;
+        }
+    }
+    
 }
